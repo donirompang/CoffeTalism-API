3fe6d5cdea3630f2ddfe84c0526b638d8430dfcf
Doni Hadiyansyah <dota.hexx@gmail.com>
2019-03-29 14:29:36 +0700

    update beans

--- blueprints/beans/__init__.py
+++ blueprints/beans/__init__.py
@@ -10,6 +10,8 @@ class Beans(db.Model):
     name = db.Column(db.String(200))
     cafeShopId = db.Column(db.Integer)
     cafeShopName = db.Column(db.String(200))
+    photoUrl = db.Column(db.String(200))
+    notes = db.Column(db.String(200))
     
 
     response_field = {
@@ -17,13 +19,17 @@ class Beans(db.Model):
         "name" : fields.String,
         "cafeShopId" : fields.Integer,
         "cafeShopName" : fields.String
+        "photoUrl" : fields.String
+        "notes" : fields.String
     }
 
-    def __init__ (self, id, name, cafeShopId, cafeShopName):
+    def __init__ (self, id, name, cafeShopId, cafeShopName, photoUrl, notes):
         self.id = id
         self.name = name
         self.cafeShopId = cafeShopId
         self.cafeShopName = cafeShopName
+        self.photoUrl = photoUrl
+        self.notes = notes
 
     def __repr__(self):
         return f'<Beans {self.id}>'
