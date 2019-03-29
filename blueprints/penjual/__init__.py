642369ec40608a26bbdb5524b04f5b284b19dc10
FahmiSyahrulah <fahmi@alphatech.id>
2019-03-29 16:22:56 +0700

    edit

--- blueprints/penjual/__init__.py
+++ blueprints/penjual/__init__.py
@@ -6,7 +6,7 @@ class Penjual(db.Model):
 
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-    username = db.Column(db.String(100))
+    username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
