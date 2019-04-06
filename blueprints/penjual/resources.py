4bcd594c41ae09ffe89ee02eb41e25719b03046d
idkaryadi <karyadi757@gmail.com>
2019-04-06 19:18:34 +0700

    fitur input reward dari penjual

--- blueprints/penjual/resources.py
+++ blueprints/penjual/resources.py
@@ -12,6 +12,7 @@ from blueprints.review import *
 from blueprints.detailtransaksi import *
 from blueprints.favorite import *
 from blueprints.pembeli import *
+from blueprints.reward import *
 
 bp_penjual = Blueprint('penjual', __name__)
 api = Api(bp_penjual)
@@ -308,7 +309,48 @@ class GetAllPushToken(Resource):
         
         return resp, 200, { 'Content-Type': 'application/json' }
 
+class RedeemRewardVoucher(Resource):
+    @jwt_required
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('code', location='json', required=True)
 
+        args = parser.parse_args()
+ 
+        cafeShopId = get_jwt_claims()['id']
+
+        qry = Reward.query.filter_by(code = args['code']).first()
+        
+        if qry is None:
+            return {'Message': 'Code tidak ditemukan'}, 404, {'Content-Type': 'application/json'}
+
+        elif args['status'] == 'used':
+            return {'Message': 'Code tidak ditemukan'}, 404, {'Content-Type': 'application/json'}
+        else:
+            qry.cafeShopId = cafeShopId
+            qry.status = "used"
+            db.session.commit()
+            resp = {}
+            resp['status'] = 200
+            resp['result'] = marshal (qry, Reward.response_field)
+            return resp, 200, { 'Content-Type': 'application/json' }
+
+class getAllRedeemRewardVoucher(Resource):
+    @jwt_required
+    def get(self):
+        penjual_id = get_jwt_claims()['id']
+        qry = Reward.query.filter_by(cafeShopId = penjual_id).all()
+        listReward = []
+        resp = {}
+        resp['status'] = 404
+        resp['results'] = []
+        if qry:
+            for row in qry:
+                reward = marshal(row, Beans.response_field)
+                listReward.append(reward)
+            resp['status'] = 200
+            resp['results'] = listProduct
+        return resp, 200, { 'Content-Type': 'application/json' }
 
 api.add_resource(EditProfilePenjual, "/api/profile/edit")
 api.add_resource(addProduct, "/api/product/tambah")
@@ -320,6 +362,9 @@ api.add_resource(addBeans, "/api/beans/tambah")
 api.add_resource(editBeans, "/api/beans/edit")
 api.add_resource(deleteBeans, "/api/beans/delete/<int:idProduct>")
 api.add_resource(getBeans, "/api/beans/get")
+
+api.add_resource(RedeemRewardVoucher, "/api/reward/put")
+api.add_resource(getAllRedeemRewardVoucher, "/api/reward/get")
 
 api.add_resource(getProfil, "/api/profil/get")
 api.add_resource(getReview, "/api/review/get")
